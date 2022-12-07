6719c04e764fb399673311071777d7a48062f69d
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2022-12-07 20:21:31 +0700

    Create KadaneAlgorithm.go

---
+++ 20221207KadaneAlgorithm/KadaneAlgorithm.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 5
+	arr := [5]int{1, 2, 3, -2, 5}
+	var sum, max int
+	max = arr[0]
+	for i := 0; i < n; i++ {
+		sum += arr[i]
+		if max < sum {
+			max = sum
+		}
+		if sum < 0 {
+			sum = 0
+		}
+	}
+	fmt.Println(max)
+}
