27c44fe85949a541ac8713b4812f088399448699
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2022-12-11 19:05:26 +0700

    Create stringAnhRune.go

---
+++ Learn/stringAndRune/stringAnhRune.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	const s = "日本語"
+
+	fmt.Println("Rune count:", utf8.RuneCountInString(s))
+	fmt.Println("\nUsing DecodeRuneInString")
+	for i, w := 0, 0; i < len(s); i += w {
+		runeValue, width := utf8.DecodeRuneInString(s[i:])
+		fmt.Printf("%#U starts at %d\n", runeValue, i)
+		w = width
+	}
+}
