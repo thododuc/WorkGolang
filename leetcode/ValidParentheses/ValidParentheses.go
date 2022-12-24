4c6fc163d1af9e5878c1673193c2423d6ec7ab9a
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2022-12-24 20:59:41 +0700

    Update ValidParentheses.go

--- leetcode/ValidParentheses/ValidParentheses.go
+++ leetcode/ValidParentheses/ValidParentheses.go
@@ -3,55 +3,78 @@ package main
 import "fmt"
 
 func isValid(s string) bool {
-	c := true
-	var p []int
-	for i := 0; i < len(s); i++ {
-		switch s[i] {
-		case '(':
-			p = append(p, 1)
-		case '[':
-			p = append(p, 2)
-		case '{':
-			p = append(p, 3)
-		case ')':
-			if len(p) == 0 {
-				c = false
-				return c
-			} else if p[len(p)-1] == 1 {
-				p = p[:len(p)-1]
+	tr := map[rune]rune{
+		'}': '{',
+		'[': ']',
+		'(': ')',
+	}
+	var p []rune
+	for _, ch := range s {
+		switch ch {
+		case '(', '[', '{':
+			p = append(p, ch)
+		case ')', ']', '}':
+			if len(p) == 0 || p[len(p)-1] != tr[ch] {
+				return false
 			} else {
-				c = false
-				break
-			}
-		case ']':
-			if len(p) == 0 {
-				c = false
-				return c
-			} else if p[len(p)-1] == 2 {
 				p = p[:len(p)-1]
-			} else {
-				c = false
-				break
-			}
-		case '}':
-			if len(p) == 0 {
-				c = false
-				return c
-			} else if p[len(p)-1] == 3 {
-				p = p[:len(p)-1]
-			} else {
-				c = false
-				break
 			}
 		}
 	}
-	if len(p) != 0 {
-		c = false
-	}
-	return c
+	return len(p) == 0
 }
 
 func main() {
 	s := "(){}}{"
 	fmt.Println(isValid(s))
 }
+
+// Day la loi giai ban dau cua minh. Y tuong thi trung khop nhung code dai dong hon nhieu.
+// func isValid(s string) bool {
+// 	c := true
+// 	var p []int
+// 	for i := 0; i < len(s); i++ {
+// 		switch s[i] {
+// 		case '(':
+// 			p = append(p, 1)
+// 		case '[':
+// 			p = append(p, 2)
+// 		case '{':
+// 			p = append(p, 3)
+// 		case ')':
+// 			if len(p) == 0 {
+// 				c = false
+// 				return c
+// 			} else if p[len(p)-1] == 1 {
+// 				p = p[:len(p)-1]
+// 			} else {
+// 				c = false
+// 				break
+// 			}
+// 		case ']':
+// 			if len(p) == 0 {
+// 				c = false
+// 				return c
+// 			} else if p[len(p)-1] == 2 {
+// 				p = p[:len(p)-1]
+// 			} else {
+// 				c = false
+// 				break
+// 			}
+// 		case '}':
+// 			if len(p) == 0 {
+// 				c = false
+// 				return c
+// 			} else if p[len(p)-1] == 3 {
+// 				p = p[:len(p)-1]
+// 			} else {
+// 				c = false
+// 				break
+// 			}
+// 		}
+// 	}
+// 	if len(p) != 0 {
+// 		c = false
+// 	}
+// 	return c
+// }
