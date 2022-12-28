157ea01dd14e3bec9204fac95d36873e8b0e9680
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2022-12-29 00:48:00 +0700

    heapify

---
+++ leetcode/1962RemoveStonestoMinimizetheTotal/RemoveStoneToMinimizeTheTotal.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func minStoneSum(piles []int, k int) int {
+	sort.Sort(sort.Reverse(sort.IntSlice(piles)))
+	fmt.Println(piles)
+	for i := 0; i < k; i++ {
+		piles[0] -= piles[0] / 2
+		heapify(piles, len(piles), 0)
+	}
+	var sum int
+	for i := 0; i < len(piles); i++ {
+		sum += piles[i]
+	}
+	return sum
+}
+
+func heapify(arr []int, n int, i int) {
+	largest := i
+	left := i*2 + 1
+	right := i*2 + 2
+	if left < n && arr[left] > arr[largest] {
+		largest = left
+	}
+	if right < n && arr[right] > arr[largest] {
+		largest = right
+	}
+	if largest != i {
+		temp := 0
+		temp = arr[i]
+		arr[i] = arr[largest]
+		arr[largest] = temp
+		heapify(arr, n, largest)
+	}
+}
+
+func main() {
+	var piles = []int{5, 4, 9}
+	var k = 2
+	fmt.Println(minStoneSum(piles, k))
+	var piles1 = []int{4, 3, 6, 7}
+	var k1 = 3
+	fmt.Println(minStoneSum(piles1, k1))
+	var piles2 = []int{1, 3, 5, 7}
+	var k2 = 3
+	fmt.Println(minStoneSum(piles2, k2))
+}
--- lego/insertToSlice/insertToSlice.go
+++ lego/insertToSlice/insertToSlice.go
@@ -1,13 +1,18 @@
-package main
-
-import "fmt"
-
-func main() {
-	slice1 := []int{1, 3, 4, 5}
-	slice2 := []int{2, 4, 6, 8}
-	// slice1 = insert(slice1, 1, slice2[2])
-	slice1 = append(slice1[:2], slice1[1:]...)
-	slice1[1] = slice2[2]
-
-	fmt.Println(slice1) // [1 6 3 4 5]
+func heapify(arr []int, n int, i int) {
+	largest := i
+	left := i*2 + 1
+	right := i*2 + 2
+	if left < n && arr[left] > arr[largest] {
+		largest = left
+	}
+	if right < n && arr[right] > arr[largest] {
+		largest = right
+	}
+	if largest != i {
+		temp := 0
+		temp = arr[i]
+		arr[i] = arr[largest]
+		arr[largest] = temp
+		heapify(arr, n, largest)
+	}
 }
