111085297d3164332f245eec634c585bf2c7e699
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2022-12-31 19:12:19 +0700

    Update 66PlusOne.go

--- leetcode/66PlusOne/66PlusOne.go
+++ leetcode/66PlusOne/66PlusOne.go
@@ -1,21 +1,23 @@
+//https://leetcode.com/problems/plus-one/description/
+
 package main
 
 import "fmt"
 
 func plusOne(digits []int) []int {
-	for i := len(digits); ; {
-		i--
-		if i == -1 {
-			digits = append([]int{1}, digits...)
-			return (digits)
-		}
-		if digits[i] == 9 {
-			digits[i] = 0
-		} else {
+	for i := len(digits) - 1; i >= 0; i-- {
+
+		if digits[i] < 9 {
 			digits[i]++
 			return (digits)
+		} else {
+			digits[i] = 0
 		}
 	}
+	if digits[0] == 0 {
+		digits = append([]int{1}, digits...)
+	}
+	return (digits)
 }
 
 func main() {
