e1c3c3fcb20e89a4808efbd352d0793c26bd646b
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-04 00:07:22 +0700

    Create FindPivotIndex.go

---
+++ leetcode/230101FindPivotIndex/FindPivotIndex.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func pivotIndex(nums []int) int {
+	sum := 0
+	for _, v := range nums {
+		sum += v
+	}
+	sumLeft := 0
+	for k, v := range nums {
+		if sumLeft*2 == sum-nums[k] {
+			return k
+		} else {
+			sumLeft += v
+		}
+	}
+	return -1
+}
+
+func main() {
+	nums := []int{-1, -1, -1, -1, -1, -1}
+	fmt.Println(pivotIndex(nums))
+}
