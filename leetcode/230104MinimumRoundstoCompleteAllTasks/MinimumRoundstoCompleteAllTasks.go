8f319dd7a0926365ed5493bbda7c7700c18786ba
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-04 22:50:22 +0700

    Update MinimumRoundstoCompleteAllTasks.go

--- leetcode/230104MinimumRoundstoCompleteAllTasks/MinimumRoundstoCompleteAllTasks.go
+++ leetcode/230104MinimumRoundstoCompleteAllTasks/MinimumRoundstoCompleteAllTasks.go
@@ -1,27 +1,63 @@
-package main
+package main //https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/description/
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+// func minimumRounds(tasks []int) int {
+// 	r := 0
+// 	m := make(map[int]int)
+// 	for _, v := range tasks {
+// 		m[v]++
+// 	}
+// 	for _, v := range m {
+// 		if v == 1 {
+// 			return -1
+// 		}
+// 		if v%3 == 0 {
+// 			r += v / 3
+// 		} else {
+// 			r += v/3 + 1
+// 		}
+// 	}
+// 	return r
+// }
 
 func minimumRounds(tasks []int) int {
-	r := 0
-	m := make(map[int]int)
-	for _, v := range tasks {
-		m[v]++
+	if len(tasks) == 1 {
+		return -1
 	}
-	for _, v := range m {
-		if v == 1 {
-			return -1
-		}
-		if v%3 == 0 {
-			r += v / 3
+	sort.Ints(tasks)
+	count := 1
+	r := 0
+	for i := 1; i < len(tasks); i++ {
+		if tasks[i] > tasks[i-1] {
+			if count == 1 {
+				return -1
+			}
+			if count%3 == 0 {
+				r += count / 3
+			} else {
+				r += count/3 + 1
+			}
+			count = 1
 		} else {
-			r += v/3 + 1
+			count++
 		}
+	}
+	if count == 1 {
+		return -1
+	}
+	if count%3 == 0 {
+		r += count / 3
+	} else {
+		r += count/3 + 1
 	}
 	return r
 }
 
 func main() {
-	tasks := []int{2, 3, 3}
+	tasks := []int{2, 2, 3, 3, 2, 4, 4, 4, 4, 4}
 	fmt.Println(minimumRounds(tasks))
 }
