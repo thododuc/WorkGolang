e8660d545d90e269f3bbcb44e2a9e135ef9e79b3
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-06 21:25:56 +0700

    update

--- 0001/0001.go
+++ 0001/0001.go
@@ -6,43 +6,47 @@ package main
 import "fmt"
 
 func canJump(nums []int) bool {
+	if len(nums) == 0 {
+		return false
+	}
 
-	var result = true
-	for i := 0; i < len(nums); i++ {
-		if i == len(nums)-1 {
-			return true
-		} else if nums[i] == 0 {
-			result = false
-			if i == 0 {
-				return result
-			}
-			need := 1
-			for j := i - 1; j >= 0; j-- {
-				if nums[j] > need {
-					result = true
+	if len(nums) == 1 {
+		return true
+	}
+
+	lastIdx := len(nums) - 1
+
+	record := make(map[int]bool)
+
+	for i := lastIdx - 1; i > -1; i-- {
+		if nums[i] >= lastIdx-i {
+			record[i] = true
+		} else {
+			for key, _ := range record {
+				if nums[i] >= key-i {
+					record[i] = true
 					break
 				}
-				if j == 0 {
-					return result
-				}
-				need++
 			}
 		}
 	}
-	return result
+
+	return record[0]
 }
 
 func main() {
-	nums := []int{2, 0, 0}
-	nums1 := []int{0}
-	nums2 := []int{0, 1}
-	nums3 := []int{2, 3, 1, 1, 4} //true
-	nums4 := []int{3, 2, 1, 0, 4} //false
-	nums5 := []int{2, 0, 1, 0, 1}
-	fmt.Println(nums, ":", canJump(nums))
-	fmt.Println(nums1, ":", canJump(nums1))
-	fmt.Println(nums2, ":", canJump(nums2))
-	fmt.Println(nums3, ":", canJump(nums3))
-	fmt.Println(nums4, ":", canJump(nums4))
-	fmt.Println(nums5, ":", canJump(nums5))
+	nums0 := []int{2, 3, 1, 1, 4}
+	// nums := []int{2, 0, 0}
+	// nums1 := []int{0}
+	// nums2 := []int{0, 1}
+	// nums3 := []int{2, 3, 1, 1, 4} //true
+	// nums4 := []int{3, 2, 1, 0, 4} //false
+	// nums5 := []int{2, 0, 1, 0, 1}
+	fmt.Println(nums0, ":", canJump(nums0))
+	// fmt.Println(nums, ":", canJump(nums))
+	// fmt.Println(nums1, ":", canJump(nums1))
+	// fmt.Println(nums2, ":", canJump(nums2))
+	// fmt.Println(nums3, ":", canJump(nums3))
+	// fmt.Println(nums4, ":", canJump(nums4))
+	// fmt.Println(nums5, ":", canJump(nums5))
 }
