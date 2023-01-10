1ad36064543b0a4c5759b1146e40bd82b369769a
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-10 10:36:48 +0700

    Create SameTree.go

---
+++ leetcode/230110SameTree/SameTree.go
@@ -0,0 +1,28 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	v := false
+	switch {
+	case p == nil && q == nil:
+		return true
+	case p == nil && q != nil:
+		return false
+	case p != nil && q == nil:
+		return false
+	default:
+		if p.Val == q.Val {
+			v = true
+		} else {
+			return false
+		}
+		l := isSameTree(p.Left, q.Left)
+		r := isSameTree(p.Right, q.Right)
+		return v && l && r
+	}
+}
