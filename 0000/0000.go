6cbfc4b1e04289a0d99b97cfe6125474af4372f3
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-01-13 01:29:26 +0700

    Update 0000.go

--- 0000/0000.go
+++ 0000/0000.go
@@ -4,19 +4,37 @@ import (
 	"fmt"
 )
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	left := preorderTraversal(root.Left)
+	right := preorderTraversal(root.Right)
+
+	output := make([]int, 0)
+
+	output = append(output, root.Val)
+	output = append(output, left...)
+	output = append(output, right...)
+	return output
+}
+
 func main() {
-	var i int
-	var p *int
-	var b bool
-	b = i == *p
-	b = i <= *p
-	b = i >= *p
-	i += *p
-	i -= *p
-	i *= *p
-	i /= *p
-	i %= *p
-	i = *p << uint(i)
-	i = *p >> uint(i)
-	fmt.Println(i, p, b)
+	root := TreeNode{Val: 1}
+	root.Left = &TreeNode{Val: 2}
+	root.Left.Left = &TreeNode{Val: 4}
+	root.Right = &TreeNode{Val: 3}
+	root.Right.Left = &TreeNode{Val: 5}
+	root.Right.Right = &TreeNode{Val: 6}
+
+	output := preorderTraversal(&root)
+	fmt.Println(output)
+
 }
