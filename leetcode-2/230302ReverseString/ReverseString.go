cdb15859d69e3a60a7d78c44b72a715f69496e69
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-03-02 09:33:13 +0700

    Update ReverseString.go

--- leetcode-2/230302ReverseString/ReverseString.go
+++ leetcode-2/230302ReverseString/ReverseString.go
@@ -1,5 +1,23 @@
 package main //https://leetcode.com/problems/reverse-string
 
-func reverseString(s []byte) {
+import "fmt"
 
+// func reverseString(s []byte) {
+// 	for i := 0; i <= len(s)/2-1; i++ {
+// 		s[i], s[len(s)-1-i] = s[len(s)-1-i], s[i]
+// 	}
+// }
+func reverseString(s []byte) {
+	left, right := 0, len(s)-1
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
+}
+func main() {
+	s := []byte{'h', 'e', 'l', 'l', 'o'}
+	fmt.Println(s)
+	reverseString(s)
+	fmt.Println(s)
 }
