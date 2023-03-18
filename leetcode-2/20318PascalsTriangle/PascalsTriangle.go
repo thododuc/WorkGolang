f70b6be2c98a380700b335902714f99f76c2df0d
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-03-18 19:40:43 +0700

    Create PascalsTriangle.go

---
+++ leetcode-2/20318PascalsTriangle/PascalsTriangle.go
@@ -0,0 +1,24 @@
+package main //https://leetcode.com/problems/pascals-triangle/
+
+import "fmt"
+
+func generate(numRows int) [][]int {
+	result :=[][]int{{1}}
+	if numRows == 1 {
+		return result
+	}
+	for i:=2; i<=numRows; i++ {
+		lastRow := make([]int,i)
+		lastRow[0], lastRow[i-1] = 1, 1
+		for j:=1; j <= i-2; j++ {
+			lastRow[j] = result[i-2][j-1] + result[i-2][j]
+		}
+		result = append(result, lastRow)
+	}
+	return result
+}
+
+func main() {
+	numRows := 5
+	fmt.Println(generate(numRows))
+}
