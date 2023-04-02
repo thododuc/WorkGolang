fce9c57eabcd88c4e4b41f462c5d2f868ef05777
thodd2209048 <114908027+thodd2209048@users.noreply.github.com>
2023-04-03 00:08:07 +0700

    Update ContainsDuplicateII.go
    solution using map

--- leetcode-2/ContainsDuplicateII/ContainsDuplicateII.go
+++ leetcode-2/ContainsDuplicateII/ContainsDuplicateII.go
@@ -6,17 +6,35 @@ func containsNearbyDuplicate(nums []int, k int) bool {
 	if k == 0 {
 		return false
 	}
-	for i := 0; i < len(nums); i++ {
-		k1:= k
-		for j := i + 1; j < len(nums) && k1 > 0; j++ {
-			if nums[i] == nums[j] {
-				return true
-			}
-			k1--
-		}
+	myMap := make(map[int]int)
+	for key, v := range nums {
+		_, ok := myMap[v]
+		if !ok || ok && key - myMap[v] > k {
+			myMap[v] = key
+		} else  {
+			return true
+		} 
 	}
 	return false
 }
+
+//another solution but slow
+// func containsNearbyDuplicate(nums []int, k int) bool {
+// 	if k == 0 {
+// 		return false
+// 	}
+// 	for i := 0; i < len(nums); i++ {
+// 		k1:= k
+// 		for j := i + 1; j < len(nums) && k1 > 0; j++ {
+// 			if nums[i] == nums[j] {
+// 				return true
+// 			}
+// 			k1--
+// 		}
+// 	}
+// 	return false
+// }
+
 
 func main() {
 	nums := []int{1,2,3,1}
